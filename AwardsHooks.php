77411a8a89c193000c0105e4db026e66cf0eb7da
spuds <spuds@spudsdesign.com>
2013-06-29 15:48:56 -0400

    ! Move awards profile option to its own menu option ! Add in disallow guest permissions ! Don't let guest see the profile awards

--- AwardsHooks.php
+++ AwardsHooks.php
@@ -27,46 +27,55 @@ function member_awards_profile_areas(&$profile_areas)
 {
 	// Profile Menu Hook, integrate_profile_areas, called from profile.php
 	// used to add menu items to the profile area
-	global $txt;
+	global $txt, $user_info;
 
-	$profile_areas['info']['areas'] = array_merge($profile_areas['info']['areas'],array(
-		'showAwards' => array(
-			'label' => $txt['showAwards'],
-			'file' => 'AwardsProfile.php',
-			'function' => 'showAwards',
-			'permission' => array(
-				'own' => 'profile_view_own',
-				'any' => 'profile_view_any',
-			),
-		),
-		'membersAwards' => array(
-			'file' => 'AwardsProfile.php',
-			'function' => 'membersAwards',
-			'hidden' => (isset($_GET['area']) && $_GET['area'] != "membersAwards"),
-			'permission' => array(
-				'own' => 'profile_view_own',
-				'any' => 'profile_view_any',
-			),
-		),
-		'listAwards' => array(
-			'label' => $txt['listAwards'],
-			'file' => 'AwardsProfile.php',
-			'function' => 'listAwards',
-			'permission' => array(
-				'own' => 'profile_view_own',
-				'any' => 'profile_view_any',
-			),
-		),
-		'requestAwards' => array(
-			'file' => 'AwardsProfile.php',
-			'hidden' => true,
-			'function' => 'requestAwards',
-			'permission' => array(
-				'own' => 'profile_view_own',
-				'any' => 'profile_view_any',
-			),
+	// No need to show these profile option to guests, perhaps a view_awards permissions should be added?
+	if ($user_info['is_guest'])
+		return;
+
+	member_awards_array_insert($profile_areas, 'info', array(
+		'member_awards' => array(
+			'title' => $txt['awards'],
+			'areas' => array(
+				'showAwards' => array(
+					'label' => $txt['showAwards'],
+					'file' => 'AwardsProfile.php',
+					'function' => 'showAwards',
+					'permission' => array(
+						'own' => 'profile_view_own',
+						'any' => 'profile_view_any',
+					),
+				),
+				'membersAwards' => array(
+					'file' => 'AwardsProfile.php',
+					'function' => 'membersAwards',
+					'hidden' => (isset($_GET['area']) && $_GET['area'] !== "membersAwards"),
+					'permission' => array(
+						'own' => 'profile_view_own',
+						'any' => 'profile_view_any',
+					),
+				),
+				'listAwards' => array(
+					'label' => $txt['listAwards'],
+					'file' => 'AwardsProfile.php',
+					'function' => 'listAwards',
+					'permission' => array(
+						'own' => 'profile_view_own',
+						'any' => 'profile_view_any',
+					),
+				),
+				'requestAwards' => array(
+					'file' => 'AwardsProfile.php',
+					'hidden' => true,
+					'function' => 'requestAwards',
+					'permission' => array(
+						'own' => 'profile_view_own',
+						'any' => 'profile_view_any',
+					),
+				)
+			)
 		)
-	));
+	), 'after');
 }
 
 /**
@@ -117,10 +126,14 @@ function member_awards_admin_areas(&$admin_areas)
  */
 function member_awards_load_permissions(&$permissionGroups, &$permissionList, &$leftPermissionGroups, &$hiddenPermissions, &$relabelPermissions)
 {
+	global $context;
+
 	// Permissions hook, integrate_load_permissions, called from ManagePermissions.php
-	// used to add new permisssions ... We need more permission hooks  .... e.g. loadIllegalGuestPermissions and setPermissionLevel !!
+	// used to add new permisssions ...
 	$permissionList['membergroup']['manage_awards'] = array(false, 'member_admin', 'administrate');
 	$permissionList['membergroup']['assign_awards'] = array(false, 'member_admin', 'administrate');
+
+	$context['non_guest_permissions'] = array_merge($context['non_guest_permissions'], array('manage_awards', 'assign_awards'));
 }
 
 /**
@@ -143,4 +156,34 @@ function member_awards_menu_buttons(&$buttons)
 		'href' => $scripturl . '?action=admin;area=awards;sa=main',
 		'show' => (allowedTo('manage_awards') || allowedto('assign_awards')),
 	);
+}
+
+/**
+ * Helper function to insert a menu
+ *
+ * @param array $input the array we will insert to
+ * @param string $key the key in the array
+ * @param array $insert the data to add before or after the above key
+ * @param string $where adding before or after
+ * @param bool $strict
+ */
+function member_awards_array_insert(&$input, $key, $insert, $where = 'before', $strict = false)
+{
+	$position = array_search($key, array_keys($input), $strict);
+
+	// If the key is not found, just insert as last
+	if ($position === false)
+	{
+		$input = array_merge($input, $insert);
+		return;
+	}
+
+	if ($where === 'after')
+		$position += 1;
+
+	// Insert as first
+	if ($position === 0)
+		$input = array_merge($insert, $input);
+	else
+		$input = array_merge(array_slice($input, 0, $position), $insert, array_slice($input, $position));
 }
