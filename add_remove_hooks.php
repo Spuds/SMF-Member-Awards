b59579486896e42f3123f50e9c92809f61426f36
spuds <spuds@spudsdesign.com>
2013-07-22 21:50:03 -0400

    ! minor cleanup, comments, unused global

--- add_remove_hooks.php
+++ add_remove_hooks.php
@@ -1,22 +1,16 @@
 <?php
-/**********************************************************************************
-* add_remove_hooks.php                                                            *
-***********************************************************************************
-***********************************************************************************
-* This program is distributed in the hope that it is and will be useful, but      *
-* WITHOUT ANY WARRANTIES; without even any implied warranty of MERCHANTABILITY    *
-* or FITNESS FOR A PARTICULAR PURPOSE.                                            *
-*                                                                                 *
-* This file is a simplified hook installer. It does what it is suppoed to.        *
-**********************************************************************************/
+
+/**
+ * This file is a simplified hook installer. It does what it is suppoed to.
+ */
 
 // If we have found SSI.php and we are outside of SMF, then we are running standalone.
 if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
 	require_once(dirname(__FILE__) . '/SSI.php');
 elseif (!defined('SMF')) // If we are outside SMF and can't find SSI.php, then throw an error
 	die('<b>Error:</b> Cannot install - please verify you put this file in the same place as SMF\'s SSI.php.');
-	
-global $sourcedir, $context;
+
+global $context;
 
 // Define the hooks
 $hook_functions = array(
@@ -38,6 +32,4 @@ foreach ($hook_functions as $hook => $function)
 	$call($hook, $function);
 
 if (SMF == 'SSI')
-   echo 'Congratulations! You have successfully installed the Member Award hooks';
-
-?>
+   echo 'Congratulations! You have successfully installed the integration hooks for Member Awards';
